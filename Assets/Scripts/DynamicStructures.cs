1d8c14bda1f38670de62e8544b0843ff729df07a
alvardalda <alvar.dalda@estudiantat.upc.edu>
2024-12-25 14:55:22 +0100

    Json File added

--- Assets/Scripts/DynamicStructures.cs
+++ Assets/Scripts/DynamicStructures.cs
@@ -1,45 +1,80 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Device;
 using UnityEngine.Timeline;
 using UnityEngine.UIElements;
+using static DynamicStructures;
 
 public class DynamicStructures : MonoBehaviour
 {
-    public GameObject prefab;
     Vector3[,] positionsArray;
     Animator animator;
     GameObject[] allChildren;
-    const int numberOfChildren = 17;
-    const int numberOfScreens = 3;
+    int numberOfChildren = 17;
+    int numberOfScreens = 3;
     int screen = 0;
 
-    // Start is called before the first frame update
+    public List<GameObject> prefabs;
+    public TextAsset jsonFile;
+
+    [System.Serializable]
+    public class World
+    {
+        public List<Level> levels;
+    }
+    [System.Serializable]
+    public class Level
+    {
+        public List<Block> blocks;
+    }
+    [System.Serializable]
+    public class Block
+    {
+        public int gameObjType;
+        public Pos pos;
+    }
+    [System.Serializable]
+    public class Pos
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+    
+    public World world = new World();
+
+
     void Start()
     {
-        allChildren = new GameObject[numberOfChildren];
-
-        positionsArray = new Vector3[numberOfScreens, numberOfChildren] {
-            { new Vector3(-12, 0, -4), new Vector3(-10, 0, -4), new Vector3(-8, 0, -4), new Vector3(-6, 0, -4), new Vector3(-4, 0, -4), new Vector3(-2, 0, -4), new Vector3(0, 0, -4), new Vector3(2, 0, -4), new Vector3(4, 0, -4), new Vector3(6, 0, -4), new Vector3(6, 0, -2), new Vector3(6, 0, 0), new Vector3(6, 0, 2), new Vector3(6, 0, 4), new Vector3(8, 0, 4), new Vector3(10, 0, 4), new Vector3(12, 0, 4) },
-            { new Vector3(-12, 0, -4), new Vector3(-12, 0, -2), new Vector3(-12, 0, 0), new Vector3(-12, 0, 2), new Vector3(-12, 0, 4), new Vector3(-10, 0, 4), new Vector3(-8, 0, 4), new Vector3(-6, -2, 4), new Vector3(-4, 0, 4), new Vector3(-2, 0, 4), new Vector3(0, 0, 4), new Vector3(2, -2, 4), new Vector3(4, 0, 4), new Vector3(6, 0, 4), new Vector3(8, 0, 4), new Vector3(10, -2, 4), new Vector3(12, 0, 4) },
-            { new Vector3(0, 0, 0), new Vector3(2, 0, 0), new Vector3(2, 0, 2), new Vector3(-2, 0, 0), new Vector3(-2, 0, -2), new Vector3(-2, 0, -4), new Vector3(-2, 0, -6), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0) } };
-        
-        Vector3 pos000 = new Vector3(0, 0, 0);
-        for (int i = 0; i < numberOfChildren; i++)
-        {
-            allChildren[i] = Instantiate(prefab, pos000, Quaternion.identity, this.transform);
-            
-            allChildren[i].transform.localPosition = positionsArray[screen, i];
-        }
-
         animator = GetComponent<Animator>();
-        animator.SetTrigger("rise");
+        try
+        {
+            world = JsonUtility.FromJson<World>(jsonFile.text);
+
+            numberOfScreens = world.levels.Count;
+            numberOfChildren = world.levels[screen].blocks.Count;
+
+            allChildren = new GameObject[numberOfChildren];
+            Vector3 pos000 = new Vector3(0, 0, 0);
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                allChildren[i] = Instantiate(prefabs[world.levels[screen].blocks[i].gameObjType], pos000, Quaternion.identity, this.transform);
+                allChildren[i].transform.localPosition = new Vector3(world.levels[screen].blocks[i].pos.x, world.levels[screen].blocks[i].pos.y, world.levels[screen].blocks[i].pos.z);
+            }
+
+            animator.SetTrigger("rise");
+        }
+        catch 
+        {
+            UnityEngine.Debug.Log("Json not attached or doesnt exist");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space)) 
@@ -51,10 +86,10 @@ public class DynamicStructures : MonoBehaviour
     public void HiddenObjectsChange()
     {
         screen = (screen + 1) % numberOfScreens;
-        
+
         for (int i = 0; i < allChildren.Length; i++)
         {
-            allChildren[i].transform.localPosition = positionsArray[screen,i];
+            allChildren[i].transform.localPosition = new Vector3(world.levels[screen].blocks[i].pos.x, world.levels[screen].blocks[i].pos.y, world.levels[screen].blocks[i].pos.z);
         }
         animator.SetTrigger("rise");
     }
